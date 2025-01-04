a7a4d099aef7f7503d5db15dc7451376fb5695ea
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-01-04 13:18:13 +0530

    Add admin registration functionality with form handling and data processing

--- app/controllers/OwnerAddAdmin.php
+++ app/controllers/OwnerAddAdmin.php
@@ -11,13 +11,37 @@ class OwnerAddAdmin extends Controller
         $this->view('owner/adminprofile');
     }
     public function adminregistration()
+
     {
         $this->view('owner/adminregistration');
     }
-    public function editprofile(){
+    public function editprofile()
+    {
         $this->view('owner/editprofile');
     }
-    public function deleteprofile(){
+    public function deleteprofile()
+    {
         $this->view('owner/deleteprofile');
+    }
+    public function register()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (isset($_POST['submit'])) {
+                $data = [
+
+                    'name' => $_POST['name'],
+                    'email' => $_POST['email'],
+                    'password' => $_POST['password'],
+                    'address' => $_POST['address'],
+                    'contactNumber' => $_POST['phone_number'],
+                    'nic' => $_POST['nic'],
+
+                ];
+
+                $admin = new  AdminRegistrationModel();
+                $admin->create($data);
+                $this->view('owner/addadmin');
+            }
+        }
     }
 }
