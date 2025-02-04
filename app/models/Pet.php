4a80a30fc7c99532f9498fb3b4b65b8ed5c96e10
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-02-04 11:09:35 +0530

    Update comment for getUserID function to clarify usage

--- app/models/Pet.php
+++ app/models/Pet.php
@@ -18,7 +18,7 @@ class Pet {
         'profilePicture'
     ];
     public $userID;
-    @use this function at start to get the petOwnerID:
+    // Use this function at start to get the petOwnerID:
     private function getUserID () {
         $this->userID = $_SESSION['userID'];
     }
