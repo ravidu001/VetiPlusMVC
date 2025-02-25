0983357adbec996660db91f2b25c7fa43cf73576
ravidu001 <ravidupeiris2002@gmail.com>
2025-02-25 20:53:30 +0530

    complete bckend for doctor's contact us complain page

--- app/models/systemcomplainModel.php
+++ app/models/systemcomplainModel.php
@@ -5,19 +5,28 @@
  
      protected $table = 'systemcomplain'; // Replace with your pets table name
  
-     protected $allowedColumns =[
-         
-         'email',
-         'complainID',
-         'issue',
-         'image',
- 
+     protected $allowedColumns =[  
+        'email',
+        'name',
+        'contactNumber',
+        'complainID',
+        'issue',
+        'image',
+        'adminEmail',
+        'dateTime',
+        'respond',
+        'status',
+        'reponseDateTime'
      ];
  
      public function getcomplain(){
          $this->order_column = 'complainID';
          $result = $this->findAll();
          return $result;
+     }
+
+     public function create($data) {
+        $this->insert($data);
      }
  
      
