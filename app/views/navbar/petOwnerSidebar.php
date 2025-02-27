15cc0b8cf5e6c1bb5ebf5388f19a92eadd80dd86
JohnManuel <sp.john.manuel737@gmail.com>
2025-02-27 20:27:48 +0530

    po sidebar added title, css remove unnecessary

--- app/views/navbar/petOwnerSidebar.php
+++ app/views/navbar/petOwnerSidebar.php
@@ -13,55 +13,55 @@
 
     <div class="links">
         <a href="<?= ROOT ?>/PO_home"
-        class="<?= ($current_pg == 'PO_home') ? 'active' : ''; ?>">
+        class="<?= ($current_pg == 'PO_home') ? 'active' : ''; ?>" title="Home">
             <i class="bx bxs-home bx-sm" id="homeIcon"></i>
             <label for="homeIcon" class="collapsable">Home</label>
         </a>
 
         <a href="#" id="appointmentIcon"
-        class="<?= ($current_pg == 'PO_vetAppointments' || $current_pg == 'PO_salonAppointments') ? 'active' : ''; ?>">
+        class="<?= ($current_pg == 'PO_vetAppointments' || $current_pg == 'PO_salonAppointments') ? 'active' : ''; ?>" title="Appointments">
             <i class="bx bxs-calendar-event bx-sm" ></i>
             <label for="appointmentIcon" class="collapsable">Appointments</label>
         </a>
         <div id="appointmentTypes">
             <a href="<?= ROOT ?>/PO_vetAppointments"
-            class="<?= ($current_pg == 'PO_vetAppointments') ? 'active' : ''; ?>">
+            class="<?= ($current_pg == 'PO_vetAppointments') ? 'active' : ''; ?>" title="Vet Appointments">
                 <i class="bx bxs-injection bx-sm" id="vetAppointmentIcon"></i>
                 <label for="vetAppointmentIcon" class="collapsable">Vet</label>
             </a>
             <a href="<?= ROOT ?>/PO_salonAppointments"
-            class="<?= ($current_pg == 'PO_salonAppointments') ? 'active' : ''; ?>">
+            class="<?= ($current_pg == 'PO_salonAppointments') ? 'active' : ''; ?>" title="Salon Appointments">
                 <i class="bx bxs-brush bx-sm" id="salonAppointmentIcon"></i>
                 <label for="salonAppointmentIcon" class="collapsable">Salon</label>
             </a>
         </div>
 
         <a href="#" id="otherServiceIcon"
-        class="<?= ($current_pg == 'PO_petAdoption' || $current_pg == 'PO_petBreeding') ? 'active' : ''; ?>">
+        class="<?= ($current_pg == 'PO_petAdoption' || $current_pg == 'PO_petBreeding') ? 'active' : ''; ?>" title="Other Services">
             <i class="bx bxs-chevrons-right bx-sm" ></i>
             <label for="otherServiceIcon" class="collapsable">Other Services</label>
         </a>
         <div id="otherServiceTypes">
             <a href="<?= ROOT ?>/PO_petAdoption"
-            class="<?= ($current_pg == 'PO_petAdoption') ? 'active' : ''; ?>">
+            class="<?= ($current_pg == 'PO_petAdoption') ? 'active' : ''; ?>" title="Pet Adoption">
                 <i class="bx bxs-dog bx-sm" id="otherServiceIcon"></i>
                 <label for="otherServiceIcon" class="collapsable">Pet Adoption</label>
             </a>
             <a href="<?= ROOT ?>/PO_petBreeding"
-            class="<?= ($current_pg == 'PO_petBreeding') ? 'active' : ''; ?>">
+            class="<?= ($current_pg == 'PO_petBreeding') ? 'active' : ''; ?>" title="Pet Breeding">
                 <i class="bx bxs-heart bx-sm" id="otherServiceIcon"></i>
                 <label for="otherServiceIcon" class="collapsable">Pet Breeding</label>
             </a>
         </div>
 
         <a href="<?= ROOT ?>/PO_aboutUs"
-        class="<?= ($current_pg == 'PO_aboutUs') ? 'active' : ''; ?>">
+        class="<?= ($current_pg == 'PO_aboutUs') ? 'active' : ''; ?>" title="About Us">
             <i class="bx bxs-group bx-sm" id="aboutIcon"></i>
             <label for="aboutIcon" class="collapsable">About Us</label>
         </a>
 
         <a href="<?= ROOT ?>/PO_contactUs"
-        class="<?= ($current_pg == 'PO_contactUs') ? 'active' : ''; ?>">
+        class="<?= ($current_pg == 'PO_contactUs') ? 'active' : ''; ?>" title="Contact Us">
             <i class="bx bxs-phone-call bx-sm" id="contactIcon"></i>
             <label for="contactIcon" class="collapsable">Contact Us</label>
         </a>
@@ -82,15 +82,15 @@
 </nav>
 
 <script>
-    const dropDowns = [
-        document.getElementById('appointmentIcon'),
-        document.getElementById('otherServiceIcon')
-    ];
-    dropDowns.map(x => addEventListener('click', () => {
-        setTimeout(() => {
-            x.style.backgroundColor = 'black';
-        }, 3000)
-    }))
+    // const dropDowns = [
+    //     document.getElementById('appointmentIcon'),
+    //     document.getElementById('otherServiceIcon')
+    // ];
+    // dropDowns.map(x => addEventListener('mouseover', () => {
+    //     setTimeout(() => {
+    //         x.style.backgroundColor = 'black';
+    //     }, 3000)
+    // }))
 
 
 
@@ -99,11 +99,8 @@
     const bodyClasses = document.body.classList
     
     const theme = localStorage.getItem('theme');
-    
-    // on mount
     theme && bodyClasses.add(theme);
     
-    // handlers
     const handleThemeToggle = () => {
         bodyClasses.toggle('lightTheme');
         bodyClasses.toggle('darkTheme');
