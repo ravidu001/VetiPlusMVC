7d635f91154e2345cc8a865549c28df1df9c3582
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-04-21 16:37:36 +0530

    update admin.php file

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -4,13 +4,26 @@ class Admin extends Controller
     public function index()
     {
         $userCount = $this->countUser();
-        // echo $userCount;
-        $this->view('admin/adminhome', ['userCount' => $userCount]);
+        $appointmentCount = $this->appointmentcount();
+    
+        $data = [
+            'userCount' => $userCount,
+            'appointmentCount' => $appointmentCount
+        ];
+    
+        $this->view('admin/adminhome', $data);
     }
+    
 
     public function countUser(){
         $user = new User();
         $count = $user->countUser();
+        return $count;
+    }
+
+    public  function appointmentcount(){
+        $appointment  = new AppointmentModel();
+        $count = $appointment->appointmentcount();
         return $count;
     }
 }
