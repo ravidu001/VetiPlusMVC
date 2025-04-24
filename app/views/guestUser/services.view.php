f7c0839a4103d1005e5864acadda6a782a0d10f1
JohnManuel <sp.john.manuel737@gmail.com>
2025-04-25 00:31:52 +0530

    added edu resources for guest and some minor css changes

--- app/views/guestUser/services.view.php
+++ app/views/guestUser/services.view.php
@@ -8,15 +8,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link href="<?= ROOT ?>/assets/css/guestUser/colourPalette.css" rel="stylesheet">
-        <link href="<?= ROOT ?>/assets/css/guestUser/styles.css" rel="stylesheet">
+        <link href="<?= ROOT ?>/assets/css/guestUser/guest_commonStyles.css" rel="stylesheet">
 
-        <link href="<?= ROOT ?>/assets/css/guestUser/navBar.css" rel="stylesheet">
-        <link href="<?= ROOT ?>/assets/css/guestUser/myFooter.css" rel="stylesheet">
-
-        <link href="<?= ROOT ?>/assets/css/guestUser/hero.css" rel="stylesheet">
         <link href="<?= ROOT ?>/assets/css/guestUser/servicesPage.css" rel="stylesheet">
 
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link href="<?= ROOT ?>/assets/css/boxicons/css/boxicons.min.css" rel="stylesheet">
         
     </head>
     <body>
@@ -33,14 +29,21 @@
             <img class="heroImg" src="<?= ROOT ?>/assets/images/guestUser/guestHeroes/servicesHero.png" alt="">
         </div>
 
+        <section class="dashArea">
+            <div class="texts">
+                <h2 class="dashHeader">Links to External Resources</h2>
+                <ul class="extLinks"></ul>
+            </div>
+            <img src="<?=ROOT?>/assets/images/guestUser/studious-doggo.jpg" alt="studious-doggo" class="eduDoggo">
+        </section>
+
         <div class="services">
             <div class="appointments">
                 <div class=" card">
                     <div class="text">
                         <h1>Vet Appointments</h1>
-                        <p>
-                            Book hassle-free veterinary appointments.
-                            Access top-quality care for your pets with just a few clicks.
+                        <p> Book hassle-free veterinary appointments.
+                            Access top-quality health care for your pets with just a few clicks.
                         </p>
                     </div>
                     <img src="<?= ROOT ?>/assets\images\guestUser\serviceIcons/vetAppointmentIcon.png" alt="Vet Appointments">
@@ -48,8 +51,7 @@
                 <div class=" card">
                     <div class="text">
                         <h1>Salon Appointments</h1>
-                        <p>
-                            Pamper your pet with ease.
+                        <p> Pamper your pet with ease.
                             Schedule grooming sessions at your preferred pet salons through our platform.
                         </p>
                     </div>
@@ -61,9 +63,8 @@
                 <div class=" card">
                     <div class="text">
                         <h1>Adoption</h1>
-                        <p>
-                            Find your perfect furry companion.
-                            Connect with local shelters and rescues to give a loving home to a pet in need.
+                        <p> Find your perfect furry companion.
+                            Connect with local shelters and other pet owners to give a loving home to a pet in need.
                         </p>
                     </div>
                     <img src="<?= ROOT ?>/assets\images\guestUser\serviceIcons/petAdoptionIcon.png" alt="">
@@ -71,9 +72,8 @@
                 <div class=" card">
                     <div class="text">
                         <h1>Pet Breeding</h1>
-                        <p>
-                            Responsible pet breeding made simple.
-                            Connect with reputable breeders or list your breeding services on our platform.
+                        <p> Responsible pet breeding made simple.
+                            Connect with reputable breeders or list your pet-breeding services on our platform.
                         </p>
                     </div>
                     <img src="<?= ROOT ?>/assets/images/guestUser/serviceIcons/petBreedingIcon.png" alt="">
@@ -81,8 +81,7 @@
                 <div class=" card">
                     <div class="text">
                         <h1>Unified Records</h1>
-                        <p>
-                            Maintain records on your pet in one place.
+                        <p> Maintain records on your pet in one place.
                             No longer is it needed to carry about (and misplace) your pet's record books.
                         </p>
                     </div>
@@ -94,5 +93,17 @@
         <!-- footer at page's bottom: -->
         <?php include_once '../app/views/navbar/guestFooter.php'; ?>
 
+        <script>
+            const linksObjects = [
+                {text: 'Pet Health Tips from Petlearnia', extLink: 'https://petlearnia.com/pet-product-buying-advice'},
+                {text: 'General Pet Education from ARF.org', extLink: 'https://www.arf-il.org/pet-owner-resources/pet-education'},
+                {text: 'Doggy Tips by Embark Passion Sri Lanka', extLink: 'https://embarkpassion.com/publications/doggy-tips'},
+                // {text: '', link: ''},
+            ]
+            const linksList = document.querySelector('.extLinks');
+            linksList.innerHTML = linksObjects
+                .map(x => `<li> <a href="${x.extLink}" target="_blank"> ${x.text} </a> </li>`).join('');
+
+        </script>
     </body>
 </html>
