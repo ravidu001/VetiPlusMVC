6848c6aa743c5cfe2f7f36f1e2bbb2f0ed74da84
ravidu001 <ravidupeiris2002@gmail.com>
2025-04-24 09:27:37 +0530

    Add method to retrieve surgery data with limit and sorting

--- app/models/SurgeryModel.php
+++ app/models/SurgeryModel.php
@@ -11,4 +11,11 @@ class SurgeryModel
     public function insertData($data) {
         $this->insert($data);
     }
+
+    public function getSurgeryData() {
+        $this->limit = 100;
+        $this->order_column = 'surgeryID';
+        $this->order_type = 'DESC';
+        return $this->findAll();
+    }
 }
