4c6d42b94e83831fafd8d7bfd55c2fdfe8a200dc
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-04-26 07:55:03 +0530

    updated file

--- app/models/VaccineDataModel.php
+++ app/models/VaccineDataModel.php
@@ -13,4 +13,15 @@ class VaccineDataModel
         $this->order_type = "asc";
         return $this->findAll();
     }
+
+    public function addNew($data){
+        $addSuccess = $this->insert($data);
+        return empty($addSuccess) ? true : false;
+    }
+    public function getalldata() {
+        $this->limit=1000;
+        $this->order_column = 'vaccineID';
+        $this->order_type = 'ASC';
+        return $this->findAll();
+    }
 }
